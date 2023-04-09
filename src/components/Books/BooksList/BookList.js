ce7298a36a1f346527dc268751a6e732daf25b27
Petar <2001petarstojanovic@gmail.com>
2023-04-09 18:49:12 +0200

    Added pagination

--- src/components/Books/BooksList/BookList.js
+++ src/components/Books/BooksList/BookList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import BookTerm from "../BookTerm/BookTerm";
+import ReactPaginate from "react-paginate";
 
 class BookList extends React.Component {
 
@@ -9,11 +10,16 @@ class BookList extends React.Component {
 
     this.state = {
       page: 0,
-      size: 20,
+      size: 5,
     }
   }
 
   render() {
+
+    const offset = this.state.size * this.state.page;
+    const nextPageOffset = offset + this.state.size
+    const pageCount = Math.ceil(this.props.books.length / this.state.size);
+    const books = this.getBooksPage(offset, nextPageOffset);
 
     return (
       <div className={"container mm-4 mt-5"}>
@@ -31,14 +37,14 @@ class BookList extends React.Component {
               </tr>
               </thead>
               <tbody>
-              {/*{books}*/}
+              {books}
 
-              {this.props.books.map((term, index) => {
-                return <BookTerm key={index} term={term}
-                                 onDelete={this.props.onDelete}
-                                 onEdit={this.props.onEdit}
-                                 onBookRent={this.props.onBookRent}/>
-              })}
+              {/*{this.props.books.map((term, index) => {*/}
+              {/*  return <BookTerm key={index} term={term}*/}
+              {/*                   onDelete={this.props.onDelete}*/}
+              {/*                   onEdit={this.props.onEdit}*/}
+              {/*                   onBookRent={this.props.onBookRent}/>*/}
+              {/*})}*/}
 
               </tbody>
             </table>
@@ -53,12 +59,47 @@ class BookList extends React.Component {
             </div>
           </div>
 
+          <ReactPaginate previousLabel={"back"}
+                         previousLinkClassName={"text-decoration-none"}
+                         nextLinkClassName={"text-decoration-none"}
+                         nextLabel={"next"}
+                         breakLabel={<a href="/#">...</a>}
+                         breakClassName={"break-me"}
+                         pageClassName={"mx-1"}
+                         pageCount={pageCount}
+                         marginPagesDisplayed={2}
+                         pageRangeDisplayed={5}
+                         onPageChange={this.handlePageClick}
+                         containerClassName={"pagination m-4 justify-content-center"}
+                         activeClassName={"active"}/>
+
         </div>
       </div>
     )
+  }
 
+  handlePageClick = (data) => {
+    let selected = data.selected;
+    this.setState({
+      page: selected
+    })
 
   }
+
+  getBooksPage = (offset, nextPageOffset) => {
+    return this.props.books.map((term, index) => {
+      return (
+        <BookTerm key={index} term={term}
+                  onDelete={this.props.onDelete}
+                  onEdit={this.props.onEdit}
+                  onBookRent={this.props.onBookRent}/>
+      );
+    }).filter((product, index) => {
+      return index >= offset && index < nextPageOffset;
+    })
+  }
+
+
 }
 
 export default BookList;
